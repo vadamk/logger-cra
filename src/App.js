7c8df178403fd4fe4100df90e6a15ff119f1c578
vadamk <volodymyr.kravch@gmail.com>
2021-03-30 10:27:19 +0700

    finish logger data representation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import dayjs from 'dayjs'
+import clsx from 'clsx'
+import _ from 'lodash'
+import { makeStyles } from '@material-ui/core';
+import CachedIcon from '@material-ui/icons/Cached';
+
+import ReCharts from './ReCharts'
+
+const useStyles = makeStyles({
+  toolbar: { display: 'flex', alignItems: 'center', padding: 15 },
+  title: { display: 'flex', alignItems: 'center' },
+  chartContainer: { height: 400 },
+  updateButton: { marginRight: 10, fontSize: 40, cursor: 'pointer' },
+})
+
+const valueToSeconds = val => +Number(val / 1000).toFixed(2)
+
+const logToChartData = (logData) => logData
+  .filter(d => d.value)
+  .map(d => ({
+    date: dayjs(d.startTime).format('MMM D hh:mm'),
+    val: valueToSeconds(d.value),
+    path: d.location
+  }))
+
+const getHourlyAverage = (points) => {
+  const dateValuePoints = points.map(d => ({
+    date: dayjs(d.date).format('MM/DD HH:00'),
+    val: d.val,
+  }))
+
+  const uniqAvgDates = _
+    .uniq(dateValuePoints, 'date')
+    .map(point => point.date)
+
+  const avgPoints = uniqAvgDates.map(date => {
+    const dateValues = dateValuePoints
+      .filter(p => p.date === date)
+      .map(p => p.val)
+
+    const valuesSum = dateValues.reduce((sum, cur) => sum + Number(cur), 0)
+    return { date, val: Number(valuesSum / dateValues.length).toFixed(2 ) }
+  })
+
+  return points.map(point => {
+    const roundedDate = dayjs(point.date).format('MM/DD HH:00')
+    return {
+      ...point,
+      avg: avgPoints.find(p => p.date === roundedDate)?.val,
+    }
+  })
+}
 
 function App() {
+  const classes = useStyles()
+
+  const [data, setData] = React.useState()
+  const [loading, setLoading] = React.useState(true)
+  const [isError, setError] = React.useState(false)
+
+  const fetchData = React.useCallback(async () => {
+    try {
+      setLoading(true)
+      const response = await fetch('https://www.lysq.click/logger')
+      const data = await response.json()
+      setData(getHourlyAverage(logToChartData(data)))
+    } catch (err) {
+      setError(true)
+      console.log(err);
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  if (isError) return <h1>Error!</h1>
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.toolbar}>
+        <h1 className={classes.title}>
+          <CachedIcon
+            className={clsx(classes.updateButton, loading && 'rotating')}
+            onClick={fetchData}
+          />
+          {loading && ' Loading...'}
+        </h1>
+      </div>
+      {data && (
+        <div className={classes.chartContainer}>
+          <ReCharts data={data} />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/ReCharts.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import {
+  ComposedChart,
+  Line,
+  Area,
+  XAxis,
+  YAxis,
+  Legend,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  customTooltip: {
+    width: 200,
+    fontSize: 12,
+    backgroundColor: '#fff',
+    padding: '0 15px',
+    border: '1px solid #ccc',
+    borderRadius: 10,
+    opacity: 0.8,
+  },
+  paragraph: {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  }
+})
+
+const CustomTooltip = ({ active, payload, label }) => {
+  const classes = useStyles()
+
+  if (active && payload && payload.length) {
+    return (
+      <div className={classes.customTooltip}>
+        <p className={classes.paragraph}>
+          <b>Value:</b>{' '}{payload[0].val} ms</p>
+        <p className={classes.paragraph}>
+          <b>Date:</b>{' '}{label}</p>
+        <p className={classes.paragraph}>
+          <b>Path:</b>{' '}{payload[0].payload.path}</p>
+        <p className={classes.paragraph}>
+          <b>Avg:</b>{' '}{payload[0].payload.avg}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+function ReCharts({ data }) {
+  const [valOpacity, setValOpacity] = React.useState(1)
+  const [avgOpacity, setAvgOpacity] = React.useState(0.5)
+
+  const handleMouseEnter = (o) => {
+    if (o.dataKey === 'avg') {
+      setValOpacity(0.5)
+    }
+
+    if (o.dataKey === 'val') {
+      setAvgOpacity(0.1)
+    }
+  };
+
+  const handleMouseLeave = (o) => {
+    if (o.dataKey === 'avg') {
+      setValOpacity(1)
+    }
+
+    if (o.dataKey === 'val') {
+      setAvgOpacity(0.5)
+    }
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <ComposedChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 15,
+          right: 30,
+          left: 10,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
+        <Line type="monotone" dataKey="val" strokeOpacity={valOpacity} stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Area type="monotone" dataKey="avg" fillOpacity={avgOpacity} stroke="#FF7F50" fill="#FF7F50" dot={false} />
+      </ComposedChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default ReCharts
